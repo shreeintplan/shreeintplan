9a20bda60ea8299a93a23f77f6ce827216810811
nikhilwedspankaja <nikhilwedspankaja@gmail.com>
2017-11-28 14:54:11 +0530

    upload3
    28/11/2017 by pankaja

--- send-mail.php
+++ send-mail.php
@@ -1,29 +1,74 @@
 
 
-<?
-
-$name=$_POST['name'];
-$Email=$_POST['email'];
-$website=$_POST['url'];
-$message=$_POST['message'];
+<?php
 
 
-    
-    $body .= "Name: " . $name . "\n"; 
-    $body .= "Email: " . $Email . "\n"; 
-    $body .= "Website: " . $website . "\n"; 
-    $body .= "Message: " . $message . "\n"; 
+if(isset($_POST['email'])) {
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "pankaja.vadhivkar@ipca.com";
+    $email_subject = "Test";
+ 
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+ 
+ 
+    // validation expected data exists
+    if(!isset($_POST['name']) ||
+        !isset($_POST['email']) ||{
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+ 
+     
+ 
+    $name = $_POST['name']; // required
+    $email = $_POST['email']; // required
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+  }
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$name)) {
+    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+  }
+ 
+  if(strlen($message) < 2) {
+    $error_message .= 'The message you entered do not appear to be valid.<br />';
+  }
+ 
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+ 
+    $email_message = "Form details below.\n\n";
+ 
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+ 
+     
+ 
+    $email_message .= "Name: ".clean_string($name)."\n";
+    $email_message .= "message: ".clean_string($message)."\n";
+ 
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
 
-    //replace with your email
-    mail("test@email.com","New email",$body); 
 
   
 ?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<script>alert("Your message has been sent successfully. We will contact you shortly.");</script>
-<meta HTTP-EQUIV="REFRESH" content="0; url=index.html"> 
-
-</head>
